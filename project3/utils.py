78e8ad21d6d5e8fa335af0c0f69077ae440baf24
HenrikEide <Henrik225@gmail.com>
2023-05-06 21:46:36 +0200

    doesnt work

--- project3/utils.py
+++ project3/utils.py
@@ -18,7 +18,6 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, device=None):
     n_batch = len(train_loader)
     losses_train = []
     model.train()
-    optimizer.zero_grad(set_to_none=True)
 
     for epoch in range(1, n_epochs + 1):
 
@@ -28,12 +27,12 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, device=None):
             contexts = contexts.to(device=device)
             targets = targets.to(device=device)
 
+            optimizer.zero_grad()
             outputs = model(contexts)
 
             loss = loss_fn(outputs, targets)
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
 
             loss_train += loss.item()
 
@@ -43,6 +42,38 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, device=None):
             print('{}  |  Epoch {}  |  Training loss {:.5f}'.format(
                 datetime.now().time(), epoch, loss_train / n_batch))
     return losses_train
+
+# def train(n_epochs, optimizer, model, loss_fn, train_loader, device=None):
+#     device = set_device(device)
+
+#     n_batch = len(train_loader)
+#     losses_train = []
+#     model.train()
+#     optimizer.zero_grad(set_to_none=True)
+
+#     for epoch in range(1, n_epochs + 1):
+
+#         loss_train = 0.0
+#         for contexts, targets in train_loader:
+
+#             contexts = contexts.to(device=device)
+#             targets = targets.to(device=device)
+
+#             outputs = model(contexts)
+
+#             loss = loss_fn(outputs, targets)
+#             loss.backward()
+#             optimizer.step()
+#             optimizer.zero_grad()
+
+#             loss_train += loss.item()
+
+#         losses_train.append(loss_train / n_batch)
+
+#         if epoch == 1 or epoch % 5 == 0:
+#             print('{}  |  Epoch {}  |  Training loss {:.5f}'.format(
+#                 datetime.now().time(), epoch, loss_train / n_batch))
+#     return losses_train
 
 
 def compute_accuracy(model, loader, device=None):
